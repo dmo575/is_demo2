ad906a3c3c7e63bcecda68264178e836e59ae347
dmo <arco4@protonmail.com>
2025-07-01 23:07:33 -0600

    cleaning up

--- DTOs/UpdateUserDTO.cs
+++ DTOs/UpdateUserDTO.cs
@@ -5,9 +5,7 @@ public class UpdateUserDTO
     public Optional<string>? FirstName { get; set; }
     public Optional<string>? LastName { get; set; }
     public Optional<string>? Email { get; set; }
-
     public Optional<string?>? PhoneNumber { get; set; }
-
     public Optional<DateOnly?>? Dob { get; set; }
 
     public string Print()
